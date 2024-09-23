4f16a9e9e2204ccf911a47b8fcd8400525f0e1fe
James Kett <james2kett@gmail.com>
2024-09-23 22:26:03 +0100

    update to pawn

--- src/main/java/com/corndel/chessington/model/pieces/Pawn.java
+++ src/main/java/com/corndel/chessington/model/pieces/Pawn.java
@@ -34,87 +34,79 @@ public class Pawn implements Piece {
 
   @Override
   public List<Move> getAllowedMoves(Coordinates from, Board board) {
-    var allowedMoves = new ArrayList<Move>();
-    var noCheckAllowedMoves = new ArrayList<Move>();
+    // return the list of the allowed moves:
+    // 1. 1 in front if free in front
+    // 2. left diagonal if opposite left diagonal
+    // 3. right diagonal if opposite right diagonal
+    // 4. 2 in front if first move
+    // board:
+    //   012
+    // 0 -------- black
+    // 1 --------
+    // 2 --------
+    // 3 --------
+    // 4 --------
+    // 5 --------
+    // 6 --------
+    // 7 -------- white
 
+    // move = 1 if color = black
+    // move = -1 if color = white
+    //                r c
+    // eg: position  [2,2]
+    // white: {front:[1,2], diag_left = [1,1], diag_right = [1,3]}
+    // black: {front:[3,2], diag_left = [3,1], diag_right = [3,3]}
+
+    // front: [r+move,c], diag_left = [r+move, c-1], diag_right = [r+move, c+1], 2up = [r+2*move, c]
+
+    var allowedMoves = new ArrayList<Move>();
     int currentRow = from.getRow();
     int currentCol = from.getCol();
     int totalCol = board.getBoard()[0].length;
     int totalRow = board.getBoard().length;
 
-    boolean whitePawnOutOfBounds = from.getRow() == 0;
-    boolean blackPawnOutOfBounds = from.getRow() == totalRow-1;
+    var move = 1;
+    if (getColour() == PlayerColour.WHITE) {move = -1;}
 
-    //check if out of bound if moved
-    if (whitePawnOutOfBounds && getColour().equals(PlayerColour.WHITE))
-      return allowedMoves;
-
-    if (blackPawnOutOfBounds && getColour().equals(PlayerColour.BLACK))
-      return allowedMoves;
-
-    // check if a diagonal move can be made
-    if (currentRow - 1 >= 0 && currentCol + 1 < totalCol) {
-      Coordinates moveWhiteDiag = new Coordinates(currentRow - 1, currentCol + 1);
-      Piece result = board.get(moveWhiteDiag);
-      if (result != null) {
-        noCheckAllowedMoves.add(new Move(from, moveWhiteDiag));
+    // check one in front
+    if (currentRow + move >=0 && currentRow + move < totalRow) {
+      // valid posibility
+      Coordinates frontCoordinates = new Coordinates(currentRow + move, currentCol);
+      Piece frontPiece = board.get(frontCoordinates);
+      if (frontPiece == null) {
+        allowedMoves.add(new Move(from, frontCoordinates));
       }
     }
 
-    if (currentRow - 1 >= 0 && currentCol - 1 >= 0) {
-      Coordinates moveWhiteAntiDiag = new Coordinates(currentRow - 1, currentCol + 1);
-      Piece result1 = board.get(moveWhiteAntiDiag);
-      if (result1 != null) {
-        noCheckAllowedMoves.add(new Move(from, moveWhiteAntiDiag));
+    // check two in front
+    if ( ((getColour() == PlayerColour.WHITE && currentRow == totalRow - 2) ||
+            (getColour() == PlayerColour.BLACK && currentRow == 1)) && !allowedMoves.isEmpty()) {
+      // valid posibility
+      Coordinates frontCoordinates = new Coordinates(currentRow + 2*move, currentCol);
+      Piece frontPiece = board.get(frontCoordinates);
+      if (frontPiece == null) {
+        allowedMoves.add(new Move(from, frontCoordinates));
       }
     }
 
-    if (currentRow + 1 < totalRow && currentCol + 1 < totalCol) {
-      Coordinates moveBlackAntiDiag = new Coordinates(currentRow + 1, currentCol + 1);
-      Piece result3 = board.get(moveBlackAntiDiag);
-      if (result3 != null) {
-        noCheckAllowedMoves.add(new Move(from, moveBlackAntiDiag));
+    // check diagonal left
+    if (currentRow + move >=0 && currentRow + move < totalRow && currentCol-1>=0 && currentCol - 1 < totalCol) {
+      Coordinates diagCoordinates = new Coordinates(currentRow + move, currentCol - 1);
+      Piece diagPiece = board.get(diagCoordinates);
+      if (diagPiece != null && diagPiece.getColour() != getColour()) {
+        allowedMoves.add(new Move(from, diagCoordinates));
       }
     }
 
-    if (currentRow + 1 < totalRow && currentCol - 1 >= 0) {
-      Coordinates moveBlackDiag = new Coordinates(currentRow + 1, currentCol - 1);
-      Piece result4 = board.get(moveBlackDiag);
-      if (result4 != null){
-        noCheckAllowedMoves.add(new Move(from, moveBlackDiag));
+    // check diagonal right
+    if (currentRow + move >=0 && currentRow + move < totalRow && currentCol+1>=0 && currentCol + 1 < totalCol) {
+      Coordinates diagCoordinates = new Coordinates(currentRow + move, currentCol + 1);
+      Piece diagPiece = board.get(diagCoordinates);
+      if (diagPiece != null && diagPiece.getColour() != getColour()) {
+        allowedMoves.add(new Move(from, diagCoordinates));
       }
     }
 
-
-
-    if (getColour().equals(PlayerColour.WHITE)){
-      if (currentRow == 6 ){
-        allowedMoves.add(new Move(from, from.plus(-2, 0)));
-      }
-      allowedMoves.add(new Move(from, from.plus(-1, 0)));
-    }
-
-    else {
-      if (currentRow == 1){
-        allowedMoves.add(new Move(from, from.plus(2, 0)));
-      }
-      allowedMoves.add(new Move(from, from.plus(1, 0)));
-    }
-
-    // dont allow move if piece on that part of the board
-    for (int i = allowedMoves.size() - 1; i >= 0; i--) {
-      Move move = allowedMoves.get(i);
-      Piece piece = board.get(move.getTo());
-      if (piece != null) {
-        allowedMoves.remove(i); // Remove the move if there's a piece at the destination
-      }
-    }
-
-
-    // I had to add this as there wre some check when pawn take horizontal pieces that shouldnt apply to them.
-    allowedMoves.addAll(noCheckAllowedMoves);
-    // TODO Implement this!
-    //return List.of();
     return allowedMoves;
   }
 }
